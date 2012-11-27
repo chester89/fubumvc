931701a017d22b6289c9ed6407f6a65d2a58d0b0
Chermennov Gleb <thebitterend77@gmail.com>
2012-11-28 00:57:53 +0400

    added Windsor projects - fake implementation for now; removed reference to Castle.Core from IntegrationTesting

---
+++ src/FubuMVC.Windsor/WindsorContainerFacility.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Castle.Windsor;
+using FubuMVC.Core.Bootstrapping;
+using FubuMVC.Core.Registration.ObjectGraph;
+using FubuMVC.Core.Runtime;
+
+namespace FubuMVC.Windsor
+{
+    public class WindsorContainerFacility: IContainerFacility
+    {
+        private readonly IWindsorContainer _container;
+
+        public WindsorContainerFacility(IWindsorContainer container)
+        {
+            _container = container;
+        }
+
+        public IServiceFactory BuildFactory()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Register(Type serviceType, ObjectDef def)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Inject(Type abstraction, Type concretion)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/FubuMVC.Windsor/WindsorServiceFactory.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using FubuCore.Binding;
+using FubuMVC.Core.Behaviors;
+using FubuMVC.Core.Runtime;
+
+namespace FubuMVC.Windsor
+{
+    public class WindsorServiceFactory: IServiceFactory
+    {
+        public IActionBehavior BuildBehavior(ServiceArguments arguments, Guid behaviorId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T Get<T>()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<T> GetAll<T>()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
