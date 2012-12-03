9e0554dda7a4e7dee6487852879dd765cb44c34b
Chermennov Gleb <thebitterend77@gmail.com>
2012-12-04 00:46:41 +0400

    get rid of unnecessary class

--- src/FubuMVC.Windsor/WindsorActivator.cs
+++ src/FubuMVC.Windsor/WindsorActivator.cs
@@ -10,24 +10,6 @@ using Castle.MicroKernel.ModelBuilder;
 
 namespace FubuMVC.Windsor
 {
-    public class WindsorActivator: DefaultComponentActivator
-    {
-        public WindsorActivator(ComponentModel model, IKernel kernel, ComponentInstanceDelegate onCreation, ComponentInstanceDelegate onDestruction)
-            : base(model, kernel, onCreation, onDestruction)
-        {
-        }
-
-        protected override object InternalCreate(CreationContext context)
-        {
-            return base.InternalCreate(context);
-        }
-
-        protected override void InternalDestroy(object instance)
-        {
-            throw new NotImplementedException();
-        }
-    }
-
     public class SingletonEqualizer: IContributeComponentModelConstruction
     {
         public void ProcessModel(IKernel kernel, ComponentModel model)
